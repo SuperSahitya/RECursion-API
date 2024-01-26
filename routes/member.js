dcb8d9d85e68ff482cd2fd19c6cd948a5fa36a73
Sahitya Kumar Choubey <sahityakumarchoubey@gmail.com>
2024-01-26 16:54:25 +0530

    fixed a bug

--- routes/member.js
+++ routes/member.js
@@ -23,8 +23,8 @@ router.get("/:year", async (req, res) => {
   try {
     const year = parseInt(req.params.year, 10);
     const data = await getData(year);
-    // res.send(data);
-    console.log(data);
+    res.send(data);
+    // console.log(data);
   } catch (error) {
     res.status(500).send({ message: "Error retrieving data" });
   }
